8c08754639a95fd7dd65f9120169207631402146
Caglar Engin <smyrna.caglar@gmail.com>
2023-10-31 15:16:24 +0100

    Feature-1 completed

---
+++ src/test/java/com/fleet/pages/MainPage.java
@@ -0,0 +1,18 @@
+package com.fleet.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class MainPage extends BasePage {
+
+    @FindBy(className = "oro-subtitle")
+    public WebElement subTitle;
+
+    @FindBy(className = "fa-question-circle")
+    public WebElement questionMark;
+
+    @FindBy(linkText = "Learn how to use this space")
+    public WebElement learnHowToUseThisSpace;
+
+
+}
